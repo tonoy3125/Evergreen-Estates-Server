5a471b7f75e5d00b4d02647f5607c30d7fa7ebc7
tonoy3125 <shaifshajedt@gmail.com>
2023-11-26 04:28:31 +0600

    verify admin middleware added commit

--- index.js
+++ index.js
@@ -76,12 +76,30 @@ async function run() {
             }
         };
 
+        const verifyAdmin = async (req, res, next) => {
+            try {
+                const email = req.decoded.email;
+                const query = { email: email };
+                const user = await userCollection.findOne(query);
+                const isAdmin = user?.role === 'admin';
+
+                if (!isAdmin) {
+                    return res.status(403).send({ message: 'forbidden access' });
+                }
+
+                next();
+            } catch (error) {
+                console.error('Error in verifyAdmin middleware:', error);
+            }
+        };
+
+
 
 
         // Users related Api
 
         // Users related Api **Get**
-        app.get('/users', verifyToken, async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
             try {
                 // const user = req.header
                 // console.log(req.headers)
