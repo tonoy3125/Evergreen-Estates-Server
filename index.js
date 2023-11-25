505bce2b82f3491855dfaab7963666b995e80499
tonoy3125 <shaifshajedt@gmail.com>
2023-11-26 03:19:22 +0600

    user delete on the backend and using delete method by id added commit

--- index.js
+++ index.js
@@ -92,6 +92,18 @@ async function run() {
             }
         });
 
+        // User Related Api *Delete*
+        app.delete('/users/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) };
+                const result = await userCollection.deleteOne(query);
+                res.send(result);
+            } catch (error) {
+                console.error("Error deleting user:", error);
+            }
+        });
+
 
 
 
