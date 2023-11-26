7cbf7efd97ce5be638d0c8e2b4c9346ac8d17c5d
tonoy3125 <shaifshajedt@gmail.com>
2023-11-26 12:45:36 +0600

    agent email token and using get method added commit

--- index.js
+++ index.js
@@ -133,6 +133,29 @@ async function run() {
             }
         });
 
+        // Agent email token
+        app.get('/user/agent/:email', verifyToken, async (req, res) => {
+            try {
+                const email = req.params.email;
+
+                if (email !== req.decoded.email) {
+                    return res.status(403).send({ message: 'forbidden access' });
+                }
+
+                const query = { email: email };
+                const user = await userCollection.findOne(query);
+                let agent = false;
+
+                if (user) {
+                    agent = user?.role === 'agent';
+                }
+
+                res.send({ agent });
+            } catch (error) {
+                console.error('Error in /user/agent/:email endpoint:', error);
+            }
+        });
+
         // Users Related Api *Post* 
         app.post('/users', async (req, res) => {
             try {
