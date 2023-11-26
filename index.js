3518f9475b5d3311e65aae21087ff7483258ff10
tonoy3125 <shaifshajedt@gmail.com>
2023-11-27 02:33:39 +0600

    get properties by id added commit

--- index.js
+++ index.js
@@ -80,6 +80,22 @@ async function run() {
         });
 
 
+        // // Properties related api
+        app.get('/properties/:id', async (req, res) => {
+            try {
+                const id = req.params.id
+                const query = { _id: new ObjectId(id) }
+                const cursor = propertyCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            } catch (error) {
+                // Handle the error
+                console.error(error)
+            }
+        })
+
+
+
 
 
         // jwt related Api
