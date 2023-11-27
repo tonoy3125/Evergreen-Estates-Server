46758a283cc071d17cf6b553d0bd5dd4a1b464c8
tonoy3125 <shaifshajedt@gmail.com>
2023-11-27 12:39:38 +0600

    properties update and using put method added commit

--- index.js
+++ index.js
@@ -94,6 +94,37 @@ async function run() {
             }
         })
 
+
+        app.put('/properties/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const filter = { _id: new ObjectId(id) }
+                const options = { upsert: true };
+                const updatedProperty = req.body
+                const property = {
+                    $set: {
+                        propertyImage: updatedProperty.propertyImage,
+                        propertyname: updatedProperty.propertyname,
+                        agentname: updatedProperty.agentname,
+                        agentemail: updatedProperty.agentemail,
+                        agentImage: updatedProperty.agentImage,
+                        location: updatedProperty.location,
+                        price: updatedProperty.price,
+                        year: updatedProperty.year,
+                        bed: updatedProperty.bed,
+                        bath: updatedProperty.bath,
+                        size: updatedProperty.size
+                    }
+                }
+                const result = await propertyCollection.updateOne(filter, property, options)
+                res.send(result)
+            } catch (error) {
+                // Handle the error
+                console.error(error);
+            }
+        })
+
+
         app.delete('/properties/:id', async (req, res) => {
             try {
                 const id = req.params.id;
