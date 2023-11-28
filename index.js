d42338eb4ca02f0634c84be961b39db89e91598e
tonoy3125 <shaifshajedt@gmail.com>
2023-11-28 16:42:12 +0600

    create payment intend and using post method added commit

--- index.js
+++ index.js
@@ -44,6 +44,32 @@ async function run() {
 
 
 
+
+        // Payment Related Api
+        // Payment Related Api **Post**
+        app.post('/create-payment-intent', async (req, res) => {
+            try {
+                const { price } = req.body;
+                const amount = parseInt(price * 100);
+                console.log(amount, 'amount inside the intent');
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amount,
+                    currency: 'usd',
+                    payment_method_types: ['card']
+                });
+                res.send({
+                    clientSecret: paymentIntent.client_secret
+                });
+            } catch (error) {
+                console.error('Error creating payment intent:', error.message);
+            }
+        });
+
+
+
+
+
+
         // propertyBrought related api
 
 
