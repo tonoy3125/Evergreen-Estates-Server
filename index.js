b4c8458ac1f27626a032cbf90644dc57f2d3a1e2
tonoy3125 <shaifshajedt@gmail.com>
2023-11-28 12:39:54 +0600

    using get method of property brought by buyer email commit

--- index.js
+++ index.js
@@ -56,6 +56,18 @@ async function run() {
                 console.error('Error fetching menu:', error);
             }
         });
+        // propertyBrought related api **Get** Buyer email
+        app.get('/propertyBroughts/:buyeremail', async (req, res) => {
+            try {
+                const find = req.params.buyeremail;
+                const query = { buyeremail: find };
+                console.log(query)
+                const result = await propertybroughtCollection.find(query).toArray()
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+            }
+        });
 
 
         // propertyBrought related api ** Post**
